3c63c70a129c60cb5896ef534fc56e8011b235bb
Jon Beltzhoover <jbeltzhoover@qci.com>
2026-06-04 10:55:52 -0500

    Updated Counter to have a subtraction button. Updated Weather to include data pulled from a public API.

--- MyBlazorProject/MyBlazorProject/Program.cs
+++ MyBlazorProject/MyBlazorProject/Program.cs
@@ -8,4 +8,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+foreach (var kvp in builder.Configuration.AsEnumerable())
+{
+    Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+}
+
+Console.WriteLine($"API Key: '{builder.Configuration["WeatherApi:ApiKey"]}'");
+
 await builder.Build().RunAsync();

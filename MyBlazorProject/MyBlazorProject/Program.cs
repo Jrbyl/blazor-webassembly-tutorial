a952009dba97f093319d180044d12a0f402e20f0
Jon Beltzhoover <jbeltzhoover@qci.com>
2026-06-05 12:32:57 -0500

    Added new page for Calculator and its associated logic.

--- MyBlazorProject/MyBlazorProject/Program.cs
+++ MyBlazorProject/MyBlazorProject/Program.cs
@@ -8,11 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-foreach (var kvp in builder.Configuration.AsEnumerable())
-{
-    Console.WriteLine($"{kvp.Key} = {kvp.Value}");
-}
-
-Console.WriteLine($"API Key: '{builder.Configuration["WeatherApi:ApiKey"]}'");
-
 await builder.Build().RunAsync();
